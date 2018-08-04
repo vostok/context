c129aa7f76c72efff1754cb406355e47fd668f78
iloktionov <iloktionov@skbkontur.ru>
2018-08-04 21:48:46 +0500

    Added code style and implemented context properties mechanism.

--- Vostok.Context/FlowingContext.cs
+++ Vostok.Context/FlowingContext.cs
@@ -1,6 +1,28 @@
-﻿namespace Vostok.Context
+﻿using System.Threading;
+using JetBrains.Annotations;
+
+namespace Vostok.Context
 {
+    /// <summary>
+    /// <para>Allows to store and retrieve arbitrary properties from ambient context.</para>
+    /// <para>Ambient context propagates by itself into synchronous and asynchronous call chains by the means of <see cref="ExecutionContext"/>.</para>
+    /// <para></para>
+    /// <para>See <see cref="Properties"/> and <see cref="IContextProperties"/> for more info on properties.</para>
+    /// <para>See <see cref="Configuration"/> and <see cref="IContextConfiguration"/> for more info on configuration.</para>
+    /// </summary>
+    [PublicAPI]
     public static class FlowingContext
     {
+        static FlowingContext()
+        {
+            Properties = new ContextProperties();
+            Configuration = new ContextConfiguration();
+        }
+
+        [NotNull]
+        public static IContextProperties Properties { get; }
+
+        [NotNull]
+        public static IContextConfiguration Configuration { get; }
     }
 }
