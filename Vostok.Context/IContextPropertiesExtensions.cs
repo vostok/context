56e2c02bf5f8dfb1b7e5b959bef0beb8b36ae5ab
iloktionov <iloktionov@skbkontur.ru>
2018-08-05 17:59:40 +0500

    IContextPropertiesExtensions: added Use() for multiple properties.

--- Vostok.Context/IContextPropertiesExtensions.cs
+++ Vostok.Context/IContextPropertiesExtensions.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 
 namespace Vostok.Context
 {
-    // TODO(iloktionov): 1. Tests
-    // TODO(iloktionov): 2. Use() for multiple properties
+    // TODO(iloktionov): Tests
 
     [PublicAPI]
     public static class IContextPropertiesExtensions
@@ -27,6 +27,78 @@ namespace Vostok.Context
             properties.Set(key, value);
 
             return new ContextPropertyScope(properties, key, oldValue, oldValueExisted);
+        }
+
+        public static IDisposable Use([NotNull] this IContextProperties properties, [NotNull] IReadOnlyDictionary<string, object> values)
+        {
+            var data = new (string, object, bool)[values.Count];
+            var dataIndex = data.Length - 1;
+
+            foreach (var pair in values)
+            {
+                var oldValueExisted = properties.Current.TryGetValue(pair.Key, out var oldValue);
+
+                data[dataIndex--] = (pair.Key, oldValue, oldValueExisted);
+
+                properties.Set(pair.Key, pair.Value);
+            }
+
+            return new ContextPropertiesScope(properties, data);
+        }
+
+        private class ContextPropertyScope : IDisposable
+        {
+            private readonly IContextProperties properties;
+            private readonly string key;
+            private readonly object oldValue;
+            private readonly bool oldValueExisted;
+
+            public ContextPropertyScope(IContextProperties properties, string key, object oldValue, bool oldValueExisted)
+            {
+                this.properties = properties;
+                this.key = key;
+                this.oldValue = oldValue;
+                this.oldValueExisted = oldValueExisted;
+            }
+
+            public void Dispose()
+            {
+                if (oldValueExisted)
+                {
+                    properties.Set(key, oldValue);
+                }
+                else
+                {
+                    properties.Remove(key);
+                }
+            }
+        }
+
+        private class ContextPropertiesScope : IDisposable
+        {
+            private readonly IContextProperties properties;
+            private readonly (string, object, bool)[] data;
+
+            public ContextPropertiesScope(IContextProperties properties, (string, object, bool)[] data)
+            {
+                this.properties = properties;
+                this.data = data;
+            }
+
+            public void Dispose()
+            {
+                foreach (var (key, oldValue, oldValueExisted) in data)
+                {
+                    if (oldValueExisted)
+                    {
+                        properties.Set(key, oldValue);
+                    }
+                    else
+                    {
+                        properties.Remove(key);
+                    }
+                }
+            }
         }
     }
 }
