aac1ea8ea534b1af16a8a7d980fb424d0f6f4598
iloktionov <iloktionov@skbkontur.ru>
2018-08-05 20:33:10 +0500

    Implemented simple binary writer and reader for strings.

---
+++ Vostok.Context/Helpers/BinaryStringReader.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Vostok.Context.Helpers
+{
+    internal class BinaryStringReader
+    {
+        private readonly byte[] buffer;
+        private int position;
+
+        public BinaryStringReader(byte[] buffer, int position = 0)
+        {
+            this.buffer = buffer;
+            this.position = position;
+        }
+
+        public bool HasDataLeft => position < buffer.Length;
+
+        public unsafe string Read()
+        {
+            int size;
+
+            fixed (byte* ptr = &buffer[position])
+                size = *(int*) ptr;
+
+            var result = Encoding.UTF8.GetString(buffer, position + sizeof(int), size);
+
+            position += sizeof(int) + size;
+
+            return result;
+        }
+    }
+}
