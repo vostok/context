c1e61507fec591d5b382763ee6aca6b31d4dd081
iloktionov <iloktionov@skbkontur.ru>
2018-08-06 23:41:07 +0500

    Added Use() extension for IContextGlobals.

---
+++ Vostok.Context.Tests/IContextGlobalsExtensions_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Context.Tests
+{
+    [TestFixture]
+    internal class IContextGlobalsExtensions_Tests
+    {
+        private IContextGlobals globals;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            globals = new ContextGlobals();
+        }
+
+        [Test]
+        public void Use_should_be_able_to_temporarily_update_the_value_of_a_global()
+        {
+            var originalValue = Guid.NewGuid().ToString();
+            var temporaryValue = Guid.NewGuid().ToString();
+
+            globals.Set(originalValue);
+
+            using (globals.Use(temporaryValue))
+            {
+                globals.Get<string>().Should().Be(temporaryValue);
+            }
+
+            globals.Get<string>().Should().Be(originalValue);
+        }
+    }
+}
