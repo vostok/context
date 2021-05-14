1db99c909bdd114ba38408fb1436545d9cff1119
deniaa <deniaa@skbkontur.ru>
2021-05-14 23:46:24 +0500

    Try to safe GC time on wasting objects

--- Vostok.Context/ContextProperties.cs
+++ Vostok.Context/ContextProperties.cs
@@ -19,6 +19,16 @@ namespace Vostok.Context
 
         public IReadOnlyDictionary<string, object> Current => Properties;
 
+        public bool SetCapacity(int capacity)
+        {
+            if (container.Value != null)
+                return false;
+
+            container.Value = new ImmutableArrayDictionary<string, object>(capacity);
+            return true;
+
+        }
+
         public bool Set(string name, object value, bool allowOverwrite = true)
         {
             if (name == null)
@@ -26,6 +36,23 @@ namespace Vostok.Context
 
             var propertiesBefore = Properties;
             var propertiesAfter = propertiesBefore.Set(name, value, allowOverwrite);
+
+            if (ReferenceEquals(propertiesBefore, propertiesAfter))
+                return false;
+
+            Properties = propertiesAfter;
+
+            return true;
+        }
+
+        public bool SetRange((string name, object value)[] newPairs, bool allowOverwrite = true)
+        {
+            for (var i = 0; i < newPairs.Length; i++)
+                if (newPairs[i].name == null)
+                    throw new ArgumentException("Keys of new pairs should not be null", nameof(newPairs));
+
+            var propertiesBefore = Properties;
+            var propertiesAfter = propertiesBefore.SetRange(newPairs, allowOverwrite);
 
             if (ReferenceEquals(propertiesBefore, propertiesAfter))
                 return false;
